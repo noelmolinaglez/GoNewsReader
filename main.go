19a7b2f52b1140c8da4a28e8bcc17767425735d1
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-06-20 19:32:09 -0400

    Querying for the news.

--- main.go
+++ main.go
@@ -36,8 +36,12 @@ func searchHandler(newsApi *news.Client) http.HandlerFunc {
 			page = "1"
 		}
 
-		fmt.Println("Search Query is: ", searchQuery)
-		fmt.Println("Page is: ", page)
+		results, err := newsApi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("%+v", results)
 	}
 }
 
