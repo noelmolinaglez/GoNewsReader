deec791e2df0cd003634248249f435fa297174c0
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-06-19 20:19:41 -0400

    Adding the search function.

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := u.Query()
+	searchQuery := params.Get("q")
+	page := params.Get("page")
+
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
+
 }
 
 func main() {
@@ -27,10 +51,9 @@ func main() {
 
 	fs := http.FileServer(http.Dir("assets"))
 
-
-
 	mux := http.NewServeMux()
-    mux.Handle("/assets/",http.StripPrefix("/assets/", fs))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	mux.HandleFunc("/search/", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 
