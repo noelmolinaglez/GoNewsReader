773b512a8b62eee95f2160349a43999552ee5090
Noel Molina Gonzalez <noelmolinaglez@gmail.com>
2021-06-20 19:27:17 -0400

    Fetching News

--- news/news.go
+++ news/news.go
@@ -1,7 +1,12 @@
 package news
 
 import (
+	"GoNewsReader/dto"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
 type Client struct {
@@ -15,4 +20,29 @@ func NewClient(httpClient *http.Client, key string, pageSize int) *Client {
 		pageSize = 100
 	}
 	return &Client{httpClient, key, pageSize}
+}
+
+func (c *Client) FetchEverything(query, page string) (*dto.NewsResult, error) {
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%s&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(query), c.PageSize, page, c.key)
+	resp, err := c.http.Get(endpoint)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(string(body))
+	}
+
+	var result dto.NewsResult
+	return &result, json.Unmarshal(body, &result)
+
 }
